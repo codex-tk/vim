23464b238818531f649243b9f1131e9c69b8afa8
codex081 <codex081@gmail.com>
2016-08-10 09:40:27 +0900

    Update ctags-boost.sh

--- ctags-boost.sh
+++ ctags-boost.sh
@@ -1,3 +1,3 @@
-sudi apt-file update
+sudo apt-file update
 apt-file list boost | grep -E -o '/usr/include/.*\.(h|hpp)' | grep -v '/usr/include/boost/typeof/' > ~/.vim/tags/boost-filelist
 ctags --sort=foldcase --c++-kinds=+p --fields=+iaS --extra=+q -f ~/.vim/tags/boost -L ~/.vim/tags/boost-filelist
